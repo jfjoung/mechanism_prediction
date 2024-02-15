621aeed75c65f1013db30f30c8f126a5c0bdbee5
FongMunHong <69520909+FongMunHong@users.noreply.github.com>
2024-02-15 15:02:26 -0500

    Create setup_gpu.sh

---
+++ Transformer/scripts/setup_gpu.sh
@@ -0,0 +1,7 @@
+conda create -y -n mech
+conda activate mech
+conda install -y python=3.8 pip rdkit=2022.09.5 openjdk=11 -c conda-forge
+conda install -y pytorch==1.12.1 cudatoolkit=11.3 -c pytorch -c conda-forge
+pip install \
+  opennmt-py==1.2.0 \
+  tqdm \
