843282a49ea980bc74579f5941591fe0d60258f0
jfjoung <112639487+jfjoung@users.noreply.github.com>
2024-02-15 17:03:19 -0500

    Update beam_search_in_docker.sh

--- Transformer/scripts/beam_search_in_docker.sh
+++ Transformer/scripts/beam_search_in_docker.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
 
-docker run --shm-size=5gb --rm --gpus '"device=0"' \
-  -v "$PWD/logs":/app/augmented_transformer/logs \
-  -v "$PWD/checkpoints":/app/augmented_transformer/checkpoints \
-  -v "$PWD/results":/app/augmented_transformer/results \
-  -v "$TEST_FILE":/app/augmented_transformer/data/tmp_for_docker/raw_test.csv \
-  -v "$PROCESSED_DATA_PATH":/app/augmented_transformer/data/tmp_for_docker/processed \
-  -v "$MODEL_PATH":/app/augmented_transformer/checkpoints/tmp_for_docker \
-  -v "$TEST_OUTPUT_PATH":/app/augmented_transformer/results/tmp_for_docker \
-  -t "${ASKCOS_REGISTRY}"/forward_predictor/augmented_transformer:1.0-gpu \
-  python at_beam_searcher.py \
+python at_beam_searcher.py \
   --model_name="augmented_transformer" \
   --data_name="$DATA_NAME" \
   --log_file="augmented_transformer_predict_$DATA_NAME" \
-  --test_file=/app/augmented_transformer/data/tmp_for_docker/raw_test.csv \
-  --processed_data_path=/app/augmented_transformer/data/tmp_for_docker/processed \
-  --model_path=/app/augmented_transformer/checkpoints/tmp_for_docker \
-  --test_output_path=/app/augmented_transformer/results/tmp_for_docker \
+  --test_file= "$TEST_FILE" \
+  --processed_data_path="$PROCESSED_DATA_PATH" \
+  --model_path="$MODEL_PATH" \
+  --test_output_path="$TEST_OUTPUT_PATH" \
   --test_unseen_name=$(basename "$TEST_FILE" | cut -d '.' -f 1) \
   --aug_factor=5 \
   -batch_size 16 \
